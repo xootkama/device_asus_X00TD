7651f7dd8343254545154060deac34731d301c33
X00T Best Kernels <88646493+xootkama@users.noreply.github.com>
2021-09-27 18:29:26 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_X00TD.mk
+    $(LOCAL_DIR)/yaap_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_X00TD-user \
-    corvus_X00TD-userdebug \
-    corvus_X00TD-eng
+    yaap_X00TD-user \
+    yaap_X00TD-userdebug \
+    yaap_X00TD-eng
